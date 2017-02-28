a708cfdf71ddc028e27f577183b2ea7b246cec8d
hudikm <hudikm@gmail.com>
2017-02-28 18:11:17 +0100

    Stage 2 - list fragment vytvoreny

--- app/src/main/java/fri/uniza/sk/fragmentlecture/MainActivity.java
+++ app/src/main/java/fri/uniza/sk/fragmentlecture/MainActivity.java
@@ -1,16 +1,32 @@
 package fri.uniza.sk.fragmentlecture;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import fri.uniza.sk.fragmentlecture.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Main activity");
+
+        ItemFragment itemFragment = ItemFragment.newInstance(1);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragmentTop,itemFragment);
+        fragmentTransaction.commit();
+
+
+
     }
 
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
